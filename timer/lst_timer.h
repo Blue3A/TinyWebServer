e976e5337b7295be9af5597deaf8fd609d180ac2
qinguoyi <1532979219@qq.com>
2019-07-08 20:28:11 +0800

    重写双向链表

--- timer/lst_timer.h
+++ timer/lst_timer.h
@@ -8,174 +8,158 @@
 class util_timer;
 struct client_data
 {
-    sockaddr_in address;
-    int sockfd;
-    char buf[ BUFFER_SIZE ];
-    util_timer* timer;
+	sockaddr_in address;
+	int sockfd;
+	char buf[BUFFER_SIZE];
+	util_timer* timer;
 };
 
 class util_timer
 {
 public:
-    util_timer() : prev( NULL ), next( NULL ){}
+	util_timer() : prev(NULL), next(NULL){}
 
 public:
-   time_t expire; 
-   void (*cb_func)( client_data* );
-   client_data* user_data;
-   util_timer* prev;
-   util_timer* next;
+	time_t expire;
+	void(*cb_func)(client_data*);
+	client_data* user_data;
+	util_timer* prev;
+	util_timer* next;
 };
 
 class sort_timer_lst
 {
 public:
-    sort_timer_lst() : head( NULL ), tail( NULL ) {}
-    ~sort_timer_lst()
-    {
-        util_timer* tmp = head;
-        while( tmp )
-        {
-            head = tmp->next;
-            delete tmp;
-            tmp = head;
-        }
-    }
-    void add_timer( util_timer* timer )
-    {
-        if( !timer )
-        {
-            return;
-        }
-        if( !head )
-        {
-            head = tail = timer;
-            return; 
-        }
-        if( timer->expire < head->expire )
-        {
-            timer->next = head;
-            head->prev = timer;
-            head = timer;
-            return;
-        }
-        add_timer( timer, head );
-    }
-    void adjust_timer( util_timer* timer )
-    {
-        if( !timer )
-        {
-            return;
-        }
-        util_timer* tmp = timer->next;
-        if( !tmp || ( timer->expire < tmp->expire ) )
-        {
-            return;
-        }
-        if( timer == head )
-        {
-            head = head->next;
-            head->prev = NULL;
-            timer->next = NULL;
-            add_timer( timer, head );
-        }
-        else
-        {
-            timer->prev->next = timer->next;
-            timer->next->prev = timer->prev;
-            add_timer( timer, timer->next );
-        }
-    }
-    void del_timer( util_timer* timer )
-    {
-        if( !timer )
-        {
-            return;
-        }
-        if( ( timer == head ) && ( timer == tail ) )
-        {
-            delete timer;
-            head = NULL;
-            tail = NULL;
-            return;
-        }
-        if( timer == head )
-        {
-            head = head->next;
-            head->prev = NULL;
-            delete timer;
-            return;
-        }
-        if( timer == tail )
-        {
-            tail = tail->prev;
-            tail->next = NULL;
-            delete timer;
-            return;
-        }
-        timer->prev->next = timer->next;
-        timer->next->prev = timer->prev;
-        delete timer;
-    }
-    void tick()
-    {
-        if( !head )
-        {
-            return;
-        }
-        //printf( "timer tick\n" );
-	LOG_INFO("%s","timer tick");
-        Log::get_instance()->flush();
-        time_t cur = time( NULL );
-        util_timer* tmp = head;
-        while( tmp )
-        {
-            if( cur < tmp->expire )
-            {
-                break;
-            }
-            tmp->cb_func( tmp->user_data );
-            head = tmp->next;
-            if( head )
-            {
-                head->prev = NULL;
-            }
-            delete tmp;
-            tmp = head;
-        }
-    }
+	sort_timer_lst(){
+		head = new util_timer;
+		tail = new util_timer;
+
+		if (NULL == head || NULL == tail){
+			exit(0);
+		}
+
+		head->next = tail;
+		head->prev = NULL;
+		tail->next = NULL;
+		tail->prev = head;
+	}
+	~sort_timer_lst()
+	{
+		util_timer *p = head;
+		util_timer *q = NULL;
+
+		while (p){
+			q = p->next;
+			delete p;
+			p = q;
+		}
+	}
+
+	//将目标定时器插入到双向链表中
+	void add_timer(util_timer* timer)
+	{
+		if (!timer)
+		{
+			return;
+		}
+
+		if (head->next == tail)
+		{
+			timer->prev = head;
+			timer->next = tail;
+			tail->prev = timer;
+			head->next = timer;
+		}
+		else
+		{
+			//按照升序将目标定时器插入到双向链表中
+			util_timer *pCurrent = head->next;
+			util_timer *pPer = head;
+
+			while (pCurrent->next && pCurrent->expire < timer->expire){
+				pPer = pCurrent;
+				pCurrent = pCurrent->next;
+			}
+
+			timer->prev = pPer;
+			timer->next = pPer->next;
+			pPer->next->prev = timer;
+			pPer->next = timer;
+		}
+	}
+	//调整定时器在链表中的位置，先取出，再插入
+	void adjust_timer(util_timer* timer)
+	{
+		if (!timer)
+		{
+			return;
+		}
+		//如果定时器在尾部，或者调整后仍然小于后面的定时器的时间
+		if (timer->next == tail || (timer->expire < timer->next->expire))
+		{
+			return;
+		}
+		else{
+			//先取出
+			timer->prev->next = timer->next;
+			timer->next->prev = timer->prev;
+
+			//插入
+			util_timer *pCurrent = timer;
+			util_timer *pPer = timer->prev;
+
+			while (pCurrent->next && pCurrent->expire < timer->expire){
+				pPer = pCurrent;
+				pCurrent = pCurrent->next;
+			}
+
+			timer->prev = pPer;
+			timer->next = pPer->next;
+			pPer->next->prev = timer;
+			pPer->next = timer;
+		}
+	}
+
+	//删除定时器
+	void del_timer(util_timer* timer)
+	{
+		if (!timer)
+		{
+			return;
+		}
+
+		timer->prev->next = timer->next;
+		timer->next->prev = timer->prev;
+		delete timer;
+	}
+
+	void tick()
+	{
+		if (!head)
+		{
+			return;
+		}
+		//printf( "timer tick\n" );
+		LOG_INFO("%s", "timer tick");
+		Log::get_instance()->flush();
+		time_t cur = time(NULL);
+		util_timer* tmp = head->next;
+		while (tmp)
+		{
+			if (cur < tmp->expire)
+			{
+				break;
+			}
+			tmp->cb_func(tmp->user_data);
+			util_timer *tmpnext = tmp->next;
+			delete tmp;
+			tmp = tmpnext;
+		}
+	}
 
 private:
-    void add_timer( util_timer* timer, util_timer* lst_head )
-    {
-        util_timer* prev = lst_head;
-        util_timer* tmp = prev->next;
-        while( tmp )
-        {
-            if( timer->expire < tmp->expire )
-            {
-                prev->next = timer;
-                timer->next = tmp;
-                tmp->prev = timer;
-                timer->prev = prev;
-                break;
-            }
-            prev = tmp;
-            tmp = tmp->next;
-        }
-        if( !tmp )
-        {
-            prev->next = timer;
-            timer->prev = prev;
-            timer->next = NULL;
-            tail = timer;
-        }
-        
-    }
-
-private:
-    util_timer* head;
-    util_timer* tail;
+	util_timer* head;
+	util_timer* tail;
 };
 
 #endif
